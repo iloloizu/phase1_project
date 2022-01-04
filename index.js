ed0b8abf7ba115543fc97763fece1ff9e7fc10bd
Ilolo Izu <iloloizu97@gmail.com>
2022-01-04 16:59:06 -0600

    cleaned up some of the old functions

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// constant variables
-
-
 document.addEventListener("DOMContentLoaded", function () {
     fetch("http://localhost:3000/gifts")
         .then(resp => resp.json())
@@ -13,38 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
         })
 })
 
-//this should ultimately show the gift page, upon being clicked from the list of names.
-// function displayGiftList(giftPic) {
-//     // const span = document.createElement('span')
-//     // giftBar.append(span)
-//     // span.textContent = giftPic.name;
-
-//     let giftImage = document.getElementById("mainGift");
-//     giftImage.src = giftPic.picture;
-//     // const giftDisplayCard = document.getElementById("test12")
-//     // const giftCard = document.createElement('li')
-//     // giftCard.append(li);
-//     // console.log(giftDisplayCard);
-//     giftImage.addEventListener("click", (e) => {
-
-//         const itemGift = document.querySelector("#itemGift")
-//         const nameGift = document.querySelector("#nameGift")
-//         const priceGift = document.querySelector("#priceGift")
-//         const pictureGift = document.querySelector("#pictureGift")
-//         const linkGift = document.querySelector("#linkGift")
-//         const commentGift = document.querySelector("#commentGift")
-
-//         itemGift.innerText = item
-//         nameGift.textContent = name
-//         priceGift.textContent = giftPic.price
-//         pictureGift.src = giftPic.image
-//         linkGift.innerText = giftPic.link
-//         commentGift.innerText = giftPic.comment
-//     })
-//     // let giftName = document.getElementById("giftGetters");
-//     // giftName.textContent = giftPic.name;
-// }
-
 function nameList(giftArray) {
     let orderedNames = document.getElementById("giftGetters")
     orderedNames.innerHTML = ""
@@ -53,8 +18,6 @@ function nameList(giftArray) {
         let nameItem = document.createElement('li')
         nameItem.textContent = giftObject.name
         orderedNames.append(nameItem)
-
-        // console.log(giftObject)
 
         nameItem.addEventListener('click', (e) => {
             const { item, name, price, picture, link, comment } = giftObject
