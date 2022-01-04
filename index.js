71907e1bb1894ed8604c2ae5e4ec919188ed8f93
Ilolo Izu <iloloizu97@gmail.com>
2022-01-04 16:56:16 -0600

    js for the main objective of the webpage working.

--- index.js
+++ index.js
@@ -6,62 +6,75 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(resp => resp.json())
         .then(data => {
             // console.log(data)
-            giftList = data
+            const giftList = data
             // console.log(giftList)
-            giftList.forEach(displayGiftList);
+            // giftList.forEach(displayGiftList);
             nameList(giftList)
         })
 })
 
 //this should ultimately show the gift page, upon being clicked from the list of names.
-function displayGiftList(giftPic) {
+// function displayGiftList(giftPic) {
+//     // const span = document.createElement('span')
+//     // giftBar.append(span)
+//     // span.textContent = giftPic.name;
 
-    // const span = document.createElement('span')
-    // giftBar.append(span)
+//     let giftImage = document.getElementById("mainGift");
+//     giftImage.src = giftPic.picture;
+//     // const giftDisplayCard = document.getElementById("test12")
+//     // const giftCard = document.createElement('li')
+//     // giftCard.append(li);
+//     // console.log(giftDisplayCard);
+//     giftImage.addEventListener("click", (e) => {
 
-    // span.textContent = giftPic.name;
+//         const itemGift = document.querySelector("#itemGift")
+//         const nameGift = document.querySelector("#nameGift")
+//         const priceGift = document.querySelector("#priceGift")
+//         const pictureGift = document.querySelector("#pictureGift")
+//         const linkGift = document.querySelector("#linkGift")
+//         const commentGift = document.querySelector("#commentGift")
 
-    const { item, name, price, picture, link, comment } = giftPic
-    let giftImage = document.getElementById("mainGift");
-    giftImage.src = giftPic.picture;
+//         itemGift.innerText = item
+//         nameGift.textContent = name
+//         priceGift.textContent = giftPic.price
+//         pictureGift.src = giftPic.image
+//         linkGift.innerText = giftPic.link
+//         commentGift.innerText = giftPic.comment
+//     })
+//     // let giftName = document.getElementById("giftGetters");
+//     // giftName.textContent = giftPic.name;
+// }
 
-
-    // const giftDisplayCard = document.getElementById("test12")
-    // const giftCard = document.createElement('li')
-    // giftCard.append(li);
-    // console.log(giftDisplayCard);
-
-    giftImage.addEventListener("click", (e) => {
-
-        const itemGift = document.querySelector("#itemGift")
-        const nameGift = document.querySelector("#nameGift")
-        const priceGift = document.querySelector("#priceGift")
-        const pictureGift = document.querySelector("#pictureGift")
-        const linkGift = document.querySelector("#linkGift")
-        const commentGift = document.querySelector("#commentGift")
-
-        itemGift.innerText = item
-        nameGift.textContent = name
-        priceGift.textContent = giftPic.price
-        pictureGift.src = giftPic.image
-        linkGift.innerText = giftPic.link
-        commentGift.innerText = giftPic.comment
-
-    })
-
-    // let giftName = document.getElementById("giftGetters");
-    // giftName.textContent = giftPic.name;
-}
-
-function nameList(giftNames) {
+function nameList(giftArray) {
     let orderedNames = document.getElementById("giftGetters")
-
     orderedNames.innerHTML = ""
 
-    giftNames.forEach(giftArray => {
+    giftArray.forEach(giftObject => {
         let nameItem = document.createElement('li')
-        nameItem.textContent = giftArray.name
+        nameItem.textContent = giftObject.name
         orderedNames.append(nameItem)
+
+        // console.log(giftObject)
+
+        nameItem.addEventListener('click', (e) => {
+            const { item, name, price, picture, link, comment } = giftObject
+
+            const itemGift = document.querySelector("#itemGift")
+            const nameGift = document.querySelector("#nameGift")
+            const priceGift = document.querySelector("#priceGift")
+            const pictureGift = document.querySelector("#mainGift")
+            const linkGift = document.querySelector("#linkGift")
+            const commentGift = document.querySelector("#commentGift")
+
+            itemGift.innerText = giftObject.item
+            nameGift.textContent = giftObject.name
+            priceGift.textContent = giftObject.price
+            pictureGift.src = giftObject.picture
+            linkGift.href = giftObject.link
+            commentGift.textContent = giftObject.comment
+
+        })
     })
+
 }
 
