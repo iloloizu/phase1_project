6e7b9395231bc545ab6eb06fd293247ef9c0ea2b
Ilolo Izu <iloloizu97@gmail.com>
2022-01-03 21:48:43 -0600

    day 1 finished

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+// constant variables
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/db.json")
+    fetch("http://localhost:3000/gifts")
         .then(resp => resp.json())
         .then(data => {
             console.log(data)
-
+            giftList = data
+            console.log(giftList)
+            giftList.forEach(displayGiftList);
         })
 })
+
+//this should ultimately show the gift page, upon being clicked from the list of names.
+function displayGiftList(giftPic) {
+
+    // const span = document.createElement('span')
+    // giftBar.append(span)
+
+    // span.textContent = giftPic.name;
+
+    const { item, name, price, picture, link, comment } = giftPic
+
+    let giftImage = document.getElementById("mainGift");
+    giftImage.src = giftPic.picture;
+
+    let giftName = document.getElementById("giftGetters");
+    giftName.textContent = giftPic.name;
+}
