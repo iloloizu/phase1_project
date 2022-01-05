0e78b6c64db69aec038cb858fea37c9e02319ca4
Ilolo Izu <iloloizu97@gmail.com>
2022-01-05 11:24:43 -0600

    updated CSS and console logged the data from the form

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+submitData();
+
 document.addEventListener("DOMContentLoaded", function () {
     fetch("http://localhost:3000/gifts")
         .then(resp => resp.json())
@@ -12,6 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function nameList(giftArray) {
     let orderedNames = document.getElementById("giftGetters")
+    let newNames = document.getElementById("newGiftAdd")
+
+
     orderedNames.innerHTML = ""
 
     giftArray.forEach(giftObject => {
@@ -40,38 +45,69 @@ function nameList(giftArray) {
 }
 
 function submitData() {
-    document.querySelector('#submitForm').addEventListener('submit', (event) => {
+    //grab the form
+    const addAnotherGift = document.getElementById("submitForm")
+    // get the event listener too 
+    addAnotherGift.addEventListener('submit', (event) => {
         event.preventDefault();
-        event.target.reset();
 
-        const idkTest = document.querySelector("#comment").event.target.value;
+        const newName = event.target["nameInput"].value
+        const newItem = event.target["itemInput"].value
+        const newPrice = event.target["priceInput"].value
+        const newPicture = event.target["pictureInput"].value
+        const newLink = event.target["linkInput"].value
+        const newComment = event.target["commentInput"].value
 
-        let output = document.querySelector("newGiftAdd")
+        const newGift = {
+            name: newName,
+            item: newItem,
+            price: newPrice,
+            picture: newPicture,
+            link: newLink,
+            comment: newComment
+        }
 
-        idkTest.textContent = output
+        console.log(newGift)
 
-        console.log(event)
+        // function handleNewData(submitData) {
+        //     let orderedNames = document.getElementById("newGiftAdd")
+        //     orderedNames.innerHTML = ""
+
+        //     submitData(newGift => {
+        //         let nameItem = document.createElement('li')
+        //         nameItem.textContent = giftObject.name
+        //         orderedNames.append(nameItem)
+
+        //         nameItem.addEventListener('click', (e) => {
+        //             const { item, name, price, picture, link, comment } = giftObject
+
+        //             const itemGift = document.querySelector("#itemGift")
+        //             const nameGift = document.querySelector("#nameGift")
+        //             const priceGift = document.querySelector("#priceGift")
+        //             const pictureGift = document.querySelector("#mainGift")
+        //             const linkGift = document.querySelector("#linkGift")
+        //             const commentGift = document.querySelector("#commentGift")
+
+        //             itemGift.innerText = giftObject.item
+        //             nameGift.textContent = giftObject.name
+        //             priceGift.textContent = giftObject.price
+        //             pictureGift.src = giftObject.picture
+        //             linkGift.href = giftObject.link
+        //             commentGift.textContent = giftObject.comment
+        //         })
+        //     })
+        // }
+        // fetch("http://localhost:3000/gifts", {
+        //     headers: { "Content-Type": "application/json" },
+        //     method: "POST",
+        //     body: JSON.stringify(newGift)
+        // })
+        //     .then(resp => resp.json())
+        //     .then(newEntity => nameList(newEntity))
+        //     .catch(error => console.error(error))
+
+        // event.target.reset()
     })
 }
 
-submitData();
 
-// function testagain() {
-
-//     const helpME = document.querySelector('#submitForm').addEventListener('submit', (event) => {
-//         event.preventDefault();
-//         const giftData = {
-//             name: event.target.nameInput.value,
-//             item: event.target.artistInput.value,
-//             price: event.target.durationInput.value,
-//             picture: event.target.youtubeLinkInput.value,
-//             link: event.target.nameInput.value,
-//             comment: event.target.nameInput.value,
-//         }
-//         nameList(giftData)
-//             .then((savedSong) => {
-//                 renderSong(savedSong);
-//                 event.target.reset();
-//             })
-//     })
-// }
